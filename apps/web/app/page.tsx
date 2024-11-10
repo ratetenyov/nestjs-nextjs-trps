99e89293ec3e0d5464b0c010450688852707ca99
ratetenyov <tetenyovvv@gmail.com>
2024-11-10 10:43:32 +0400

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -4,6 +4,10 @@ import { trpc } from './trpc';
 export default async function Home() {
     const response = await trpc.hello.query({ name: 'Yasha' });
 
+    if (!response) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <p>Server side {response}</p>
