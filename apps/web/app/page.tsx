1673c4a04f77f460f5ffe03718353f200489a3a3
ratetenyov <tetenyovvv@gmail.com>
2024-11-10 10:53:46 +0400

    fix

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,7 @@
 // import ClientSide from './ClientSide';
-import { trpc } from './trpc';
+
+import { Wrapper } from './Wrapper';
 
 export default async function Home() {
-    const response = await trpc.hello.query({ name: 'Yasha' });
-
-    if (!response) {
-        return <div>Loading...</div>;
-    }
-
-    return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <p>Server side {response}</p>
-            {/* <ClientSide /> */}
-        </div>
-    );
+    return <Wrapper />;
 }
