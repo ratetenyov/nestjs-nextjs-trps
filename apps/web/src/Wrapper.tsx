e2b20356dd21f3a4179753b19b9600d5edba00e0
ratetenyov <tetenyovvv@gmail.com>
2024-11-10 11:25:27 +0400

    fix

--- apps/web/src/Wrapper.tsx
+++ apps/web/src/Wrapper.tsx
@@ -1,11 +1,6 @@
 import { useEffect, useState } from 'react';
 import { trpc } from './trpc';
 
-const getResponse = async () => {
-    const response = await trpc.hello.query({ name: 'Yasha' });
-    return response;
-};
-
 export const Wrapper = () => {
     const [response, setResponse] = useState<string>('');
 
